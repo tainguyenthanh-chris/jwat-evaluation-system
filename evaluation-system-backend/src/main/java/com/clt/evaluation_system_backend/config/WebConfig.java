75344817f90bfa79e7f17225d6c7d114b9fb886f
Nguyen Thanh Tai <168047056+thanhtai303@users.noreply.github.com>
2025-12-22 15:56:30 +0700

    feat: add swagger

--- evaluation-system-backend/src/main/java/com/clt/evaluation_system_backend/config/WebConfig.java
+++ evaluation-system-backend/src/main/java/com/clt/evaluation_system_backend/config/WebConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
@@ -14,6 +13,4 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("*")
                 .allowCredentials(true);
     }
-
 }
-
